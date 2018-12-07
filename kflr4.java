00dcbbdbaf287c2e86f67c278394011b61766a1e
venkymaganti1935 <42571497+venkymaganti1935@users.noreply.github.com>
2018-12-07 12:17:45 +0530

    Create kflr4.java

---
+++ kflr4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class JavaProgram
+{
+    public static void main(String args[])
+    {
+       int yr;
+       Scanner scan = new Scanner(System.in);
+	   
+       System.out.print("Enter Year : ");
+       yr = scan.nextInt();
+	   
+       if((yr%4 == 0) && (yr%100!=0))
+       {
+           System.out.print(true);
+       }
+       else if((yr%100 == 0) && (yr%400 == 0))
+       {
+           System.out.print(true);
+       }
+       else if(yr%400 == 0)
+       {
+           System.out.print(true);
+       }
+       else
+       {
+           System.out.print(false);
+       }
+    }
+}
