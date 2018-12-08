00c672a6b33ea3650daa799e6705b0a57661d92c
venkymaganti1935 <42571497+venkymaganti1935@users.noreply.github.com>
2018-12-08 10:56:41 +0530

    Create venky.c

---
+++ venky.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("enter the number is even");
+}
+else
+{
+printf("enter the number is odd");
+}
+return 0;
+}
